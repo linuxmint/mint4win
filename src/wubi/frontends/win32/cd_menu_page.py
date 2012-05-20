31a94995a6aa2491013337b25d4d5c995d94187c
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-05-20 23:16:34 +0100

    Don't hide the installer from the cd menu

--- src/wubi/frontends/win32/cd_menu_page.py
+++ src/wubi/frontends/win32/cd_menu_page.py
@@ -60,14 +60,14 @@ class CDMenuPage(Page):
         #wubi
         used = ((cd_drive.total_space_mb - cd_drive.free_space_mb)*1024*1024)
         # Hide Wubi from the autorun page, per bug #975251.
-        if self.info.force_wubi:
-            y += lh + sep
-            self.main.wubi_button = ui.FlatButton(self.main, x, y, bw, bh, _("Install inside Windows"))
-            y += bh + 2
-            txt = _("Install and uninstall %(distro)s like any other application, without the need for a dedicated partition. You will be able to boot into either Windows or %(distro)s. Hibernation is not enabled in this mode and disk performance is slightly reduced.")
-            txt = txt % dict(distro=distro_name)
-            self.main.wubi_label = ui.Label(self.main, x, y, lw, lh, txt)
-            self.main.wubi_button.on_click = self.on_wubi
+        #if self.info.force_wubi:
+        y += lh + sep
+        self.main.wubi_button = ui.FlatButton(self.main, x, y, bw, bh, _("Install inside Windows"))
+        y += bh + 2
+        txt = _("Install and uninstall %(distro)s like any other application, without the need for a dedicated partition. You will be able to boot into either Windows or %(distro)s. Hibernation is not enabled in this mode and disk performance is slightly reduced.")
+        txt = txt % dict(distro=distro_name)
+        self.main.wubi_label = ui.Label(self.main, x, y, lw, lh, txt)
+        self.main.wubi_button.on_click = self.on_wubi
 
         #info
         y += lh + sep
